d8d11e399d68c93d677330a304c3e2e21d54f205
Shoaib Ihsan <theshoaibihsan10@gmail.com>
2024-09-04 18:37:16 +0500

    changed to post

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/testing_webhook', methods=['GET'])
+@app.route('/testing_webhook', methods=['POST'])
 def webhook():
     # Get data from query parameters
-    data = request.args.to_dict()
+    data = request.data
     
     # Log the received data
     print(f"Received data: {data}")
 
     # Send a response back to the sender
-    return jsonify({"status": "success", "data": data}), 200
+    return jsonify({"status": "success", "data": "Hello"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True,port=80)
