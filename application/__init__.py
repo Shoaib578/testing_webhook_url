05d1e83e2b313e5cc00a940ca03794c1fc357c50
Shoaib Ihsan <theshoaibihsan10@gmail.com>
2024-09-10 00:13:22 +0500

    apis completed

--- application/__init__.py
+++ application/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Configuring SQLite database (you can use PostgreSQL, MySQL, etc. instead)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://default:6zG1WeAguJoP@ep-little-bush-a46q76gm.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
