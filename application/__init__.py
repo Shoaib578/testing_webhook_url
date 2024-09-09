d9b1993903a8f3ebdb25152da61eab104c1436af
Shoaib Ihsan <theshoaibihsan10@gmail.com>
2024-09-09 23:45:12 +0500

    completed apis

--- application/__init__.py
+++ application/__init__.py
@@ -11,3 +11,5 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Initializing the database
 db = SQLAlchemy(app)
 
+from application.routes.routes import routes
+app.register_blueprint(routes)
